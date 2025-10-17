a5be6dfd9e799c9b3f953b321e6bf30dcf9c42ef
Jesus Saucedo <23470133@capeche.tecnm.mx>
2025-10-17 10:55:07 -0600

    Agregamos comportamientos a la interfaz

--- TipCalculator.Gui/MainPage.xaml.cs
+++ TipCalculator.Gui/MainPage.xaml.cs
@@ -2,12 +2,37 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void CalcularClicked(object sender, EventArgs e)
+        {
+            decimal importe = decimal.Parse(txtImporte.Text);
+            decimal personas = decimal.Parse(txtPersonas.Text);
+            decimal propina = decimal.Parse(txtPropina.Text);
+
+            decimal propinaTotal = importe * propina / 100;
+            decimal propinaPersona = propinaTotal / personas;
+            decimal total = importe + propinaTotal;
+            decimal totalPersona = total / personas;
+
+            lblPropinaTotal.Text = propinaTotal.ToString("F2");
+            lblPropinaPorPersona.Text = propinaPersona.ToString("F2");
+            lblTotal.Text = total.ToString("F2");
+            lblTotalPorPersona.Text = totalPersona.ToString("F2");
+        }
+
+        private void LimpiarClicked(object sender, EventArgs e)
+        {
+            txtImporte.Text = "";
+            txtPersonas.Text = "";
+            txtPropina.Text = "";
+            lblPropinaTotal.Text = "0.00";
+            lblPropinaPorPersona.Text = "0.00";
+            lblTotal.Text = "0.00";
+            lblTotalPorPersona.Text = "0.00";
+        }
     }
 }
