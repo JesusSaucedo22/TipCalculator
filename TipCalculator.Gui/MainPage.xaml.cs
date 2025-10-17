b69f741e86ae6a8cfe382eeb27674d27b7304983
Jesus Saucedo <23470133@capeche.tecnm.mx>
2025-10-17 09:21:59 -0600

    Primer commit, hoja en blanco

---
+++ TipCalculator.Gui/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TipCalculator.Gui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
